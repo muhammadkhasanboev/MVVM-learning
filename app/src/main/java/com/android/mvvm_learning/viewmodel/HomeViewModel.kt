eb21d0bfe6d33e8e765cb9dd2bec7632ef3bd624
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-09-07 14:28:12 +0500

    viewmodel code

--- app/src/main/java/com/android/mvvm_learning/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/android/mvvm_learning/viewmodel/HomeViewModel.kt
@@ -1,2 +1,19 @@
 package com.android.mvvm_learning.viewmodel
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.android.mvvm_learning.model.UserData
+import com.android.mvvm_learning.model.UserRepository
+
+class HomeViewModel: ViewModel(){
+    val userRepository: UserRepository = UserRepository()
+
+    private val _userData = MutableLiveData<UserData>()
+    val userData: MutableLiveData<UserData> = _userData
+
+
+    fun getUserData(){
+        val userResult = userRepository.fetchUserData()
+        _userData.postValue(userResult)
+    }
+}
