aaf05ecc5930c24d17f2c52cf3eb04f1419fe85f
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-09-07 16:08:54 +0500

    simple model is ready

--- app/src/main/java/com/android/mvvm_learning/MainActivity.kt
+++ app/src/main/java/com/android/mvvm_learning/MainActivity.kt
@@ -11,15 +11,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
 import com.android.mvvm_learning.ui.theme.MVVMlearningTheme
+import com.android.mvvm_learning.view.HomePage
+import com.android.mvvm_learning.viewmodel.HomeViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val homeViewModel = ViewModelProvider(this) [HomeViewModel:: class]
         enableEdgeToEdge()
         setContent {
             MVVMlearningTheme {
 
+                HomePage(modifier = Modifier, homeViewModel)
             }
         }
     }
--- app/src/main/java/com/android/mvvm_learning/view/HomePage.kt
+++ app/src/main/java/com/android/mvvm_learning/view/HomePage.kt
@@ -1,9 +1,35 @@
 package com.android.mvvm_learning.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.android.mvvm_learning.viewmodel.HomeViewModel
 
 @Composable
-fun HomePage(modifier: Modifier= Modifier){
+fun HomePage(modifier: Modifier= Modifier, viewModel: HomeViewModel){
+    val userData = viewModel.userData.observeAsState()
 
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Button(
+            onClick = {
+                viewModel.getUserData()
+            }
+        ){
+            Text("get data")
+        }
+        userData.value?.name?.let{
+            Text("$it")
+        }
+
+    }
 }
